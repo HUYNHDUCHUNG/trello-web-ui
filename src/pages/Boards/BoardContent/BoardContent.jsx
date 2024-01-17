f6e4898e879692876f678f04e6f8f1369272709f
Hung <huynhhung2108@gmail.com>
2024-01-17 19:45:58 +0700

    add drag_drop_column

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,21 +1,47 @@
 import { Box } from '@mui/material'
 import ListColumns from './ListColumns/ListColumns'
 import { mapOrder } from '~/utils/sort'
-
+import { DndContext, PointerSensor, MouseSensor, TouchSensor, useSensor, useSensors } from '@dnd-kit/core'
+import { useEffect, useState } from 'react'
+import { arrayMove } from '@dnd-kit/sortable'
 function BoardContent({ board }) {
-  const orderedColums = mapOrder(board?.columns, board?.columnOrderIds, '_id')
+  // const pointerSensor = useSensor(PointerSensor, { activationConstraint: { distance: 10 } })
+  const mouseSensor = useSensor(MouseSensor, { activationConstraint: { distance: 10 } })
+  const touchSensor = useSensor(TouchSensor, { activationConstraint: { delay: 250, tolerance: 500 } })
+  const mySensors = useSensors(mouseSensor, touchSensor)
 
+  const [orderedColumns, setOrderedColumns] = useState([])
+  useEffect(() => {
+    setOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, '_id'))
+  }, [board])
+  const handleDragEnd = (event) => {
+    console.log(event)
+    const { active, over } = event
+
+    if (!over) return
+
+    if (active.id !== event.id) {
+      const oldIndex = orderedColumns.findIndex((c) => c._id === active.id)
+      const newIndex = orderedColumns.findIndex((c) => c._id === over.id)
+
+      const dndOrderedColumns = arrayMove(orderedColumns, oldIndex, newIndex)
+      // const dndOrderedColumnsId = dndOrderedColumns.map((c) => c._id)
+      setOrderedColumns(dndOrderedColumns)
+    }
+  }
   return (
-    <Box
-      sx={{
-        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#2a9dcc'),
-        height: (theme) => theme.trello.boardContentHeight,
-        width: '100%',
-        p: '10px 0'
-      }}
-    >
-      <ListColumns columns={orderedColums} />
-    </Box>
+    <DndContext onDragEnd={handleDragEnd} sensors={mySensors}>
+      <Box
+        sx={{
+          bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#2a9dcc'),
+          height: (theme) => theme.trello.boardContentHeight,
+          width: '100%',
+          p: '10px 0'
+        }}
+      >
+        <ListColumns columns={orderedColumns} />
+      </Box>
+    </DndContext>
   )
 }
 
--- src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
+++ src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
@@ -1,50 +1,53 @@
 import { Box, Button } from '@mui/material'
 import Column from './Column/Column'
 import NoteAddIcon from '@mui/icons-material/NoteAdd'
+import { SortableContext, horizontalListSortingStrategy } from '@dnd-kit/sortable'
 function ListColumns({ columns }) {
   return (
-    <Box
-      sx={{
-        bgcolor: 'inherit',
-        width: '100%',
-        height: '100%',
-        display: 'flex',
-        overflowX: 'auto',
-        overflowY: 'hidden',
-        '&::-webkit-scrollbar-track': { m: 2 }
-      }}
-    >
-      {columns?.map((column) => (
-        <Column key={column._id} column={column} />
-      ))}
-
+    <SortableContext items={columns?.map((c) => c._id)} strategy={horizontalListSortingStrategy}>
       <Box
         sx={{
-          minWidth: '200px',
-          maxWidth: '200px',
-          mx: 2,
-          borderRadius: '6px',
-          height: 'fit-content',
-          bgcolor: '#ffffff3d',
-          '&:hover': {
-            bgcolor: '#ffffff1d'
-          }
+          bgcolor: 'inherit',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          overflowX: 'auto',
+          overflowY: 'hidden',
+          '&::-webkit-scrollbar-track': { m: 2 }
         }}
       >
-        <Button
+        {columns?.map((column) => (
+          <Column key={column._id} column={column} />
+        ))}
+
+        <Box
           sx={{
-            color: 'white',
-            width: '100%',
-            justifyContent: 'flex-start',
-            pl: 2.5,
-            py: 1
+            minWidth: '200px',
+            maxWidth: '200px',
+            mx: 2,
+            borderRadius: '6px',
+            height: 'fit-content',
+            bgcolor: '#ffffff3d',
+            '&:hover': {
+              bgcolor: '#ffffff1d'
+            }
           }}
-          startIcon={<NoteAddIcon />}
         >
-          Add new column
-        </Button>
+          <Button
+            sx={{
+              color: 'white',
+              width: '100%',
+              justifyContent: 'flex-start',
+              pl: 2.5,
+              py: 1
+            }}
+            startIcon={<NoteAddIcon />}
+          >
+            Add new column
+          </Button>
+        </Box>
       </Box>
-    </Box>
+    </SortableContext>
   )
 }
 
